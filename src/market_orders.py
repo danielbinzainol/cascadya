c07854ed9738d0db472402a495bca4aa5347c076
Loris Amabile <loris.amabile@cascadya.com>
2026-02-24 18:33:03 +0100

    edit the creation of market_orders to use kw and the new unit conversion function

--- src/market_orders.py
+++ src/market_orders.py
@@ -7,9 +7,11 @@ import pandas as pd
 
 from src.ingest import data_workflow
 
-from src.utils import load_config, convert_m3_to_mwhth
+from src.utils import load_config, convert_saturated_steam_units
 from src.predict import copy_median_values
 from src.dataset import resample
+
+COEFF_ELEC_BOILER_EFFICIENCY = 1
 
 ORDER_HEADER = [
     "asset_id",
@@ -25,12 +27,12 @@ def build_market_orders(
     forecast: pd.DataFrame,
     prix_seuil_euro_mwh: float,
     puissance_chaudiere_elec_mw: float,
-    capacite_min_gaz_mwhth: float,
+    capacite_min_gaz_kwhth: float,
     *,
     timestamp_col :str = "measured_at_utc",
-    value_col: str, # must be in MWhth
+    value_col: str, # must be in kWhth
     output_dir: Path | str = Path(r"data\market_orders"),
-    run_at: datetime | pd.Timestamp | None = None,
+    run_at: datetime.datetime | pd.Timestamp | None = None,
     sep: str = ";",
 ) -> list[Path]:
     """
@@ -46,20 +48,25 @@ def build_market_orders(
     if df[timestamp_col].isna().any() | df[value_col].isna().any() :
         raise ValueError(f"NaN values in timestamp or value columns")
 
+    if "kwhth" not in value_col.split("_")[-1]:
+        raise ValueError(f"Value col {value_col} is not in kWh")
+
     # # read config info
     # config = load_config()
     # prix_seuil_euro_mwh = config[project_name]['prix_seuil_euro_mwh"]
     # puissance_chaudiere_elec_mw = config[project_name]["puissance_chaudiere_elec_mw"]
-    # capacite_min_gaz_mwhth = config[project_name]["capacite_min_gaz_mwhth"]
+    # capacite_min_gaz_kwhth = config[project_name]["capacite_min_gaz_kwhth"]
     
     # prise en compte du seuil pour la chaudière gaz
-    available_mwh = (df[value_col] - float(capacite_min_gaz_mwhth)).clip(lower=0)
-    # conversion au quart d'heure
-    available_power_mw = available_mwh * 60/15
+    available_kwhth = (df[value_col] - float(capacite_min_gaz_kwhth)).clip(lower=0)
+    # prise en compte du rendement de la chaudière élec
+    available_kwh = available_kwhth / COEFF_ELEC_BOILER_EFFICIENCY
+    # conversion en puissance sur le quart d'heure
+    available_power_kw = available_kwh * 60/15
     # prise en compte de la puissance max pour la chaudière élec
-    available_power_mw = available_power_mw.clip(upper=float(puissance_chaudiere_elec_mw))
+    available_power_kw = available_power_kw.clip(upper=float(puissance_chaudiere_elec_mw)*1000)
     # valeur finale
-    power_kw_sell = -1 * (available_power_mw * 1000)
+    power_kw_sell = -1 * available_power_kw
 
     orders = pd.DataFrame({
         "asset_id": project_name,
@@ -91,22 +98,22 @@ def complex_market_orders_data_workflow(project: str):
     df = data_workflow(project)
 
     # include the unit into the main column name
-    df = df.rename(columns={"Valeur": "steam_production_(m3/h)"})
+    df = df.rename(columns={"Valeur": "steam_production_m3/h"})
 
     # for other columns, keep only numerical columns
     # set the timestamp as a column with a standard name, not as the index
     df = df.drop(columns=["Unité"])
 
-    df = df[["measured_at_utc", "steam_production_(m3/h)"]] #required, as the data_workflow creates new columns when localizing and converting to utc
+    df = df[["measured_at_utc", "steam_production_m3/h"]] #required, as the data_workflow creates new columns when localizing and converting to utc
     df_15min = resample(df, desired_timedelta="15min", aggregate_function="mean")
-    df_15min["steam_production_(m3/h)"] = df_15min["steam_production_(m3/h)"].fillna(0)
-    df_median1week = copy_median_values(df_15min, "measured_at_utc", "steam_production_(m3/h)", respect_holidays=False, respect_weekdays=True, respect_time=True, extension="semaine")
+    df_15min["steam_production_m3/h"] = df_15min["steam_production_m3/h"].fillna(0)
+    df_median1week = copy_median_values(df_15min, "measured_at_utc", "steam_production_m3/h", respect_holidays=False, respect_weekdays=True, respect_time=True, extension="semaine")
     config = load_config()
 
-    df_median1week = convert_m3_to_mwhth(
+    df_median1week = convert_saturated_steam_units(
         df_median1week,
-        "steam_production_(m3/h)",
-        converted_value_col = "steam_production_mwhth"
+        "steam_production_m3/h",
+        converted_value_col = "steam_production_kwhth"
     )
 
     paths = build_market_orders(
@@ -114,8 +121,8 @@ def complex_market_orders_data_workflow(project: str):
         forecast=df_median1week,  # output de simple_copy / copy_median_values
         prix_seuil_euro_mwh=config[project]["prix_seuil_euro_mwh"][0],
         puissance_chaudiere_elec_mw=config[project]["puissance_chaudiere_elec_mw"],
-        capacite_min_gaz_mwhth=config[project]["capacite_min_gaz_mwhth"],
+        capacite_min_gaz_kwhth=config[project]["capacite_min_gaz_kwhth"],
         # timestamp_col="measured_at_utc",  # optionnel si non détectable
-        value_col="steam_production_mwhth",  # optionnel si non détectable
+        value_col="steam_production_kwhth",  # optionnel si non détectable
     )
     return paths
