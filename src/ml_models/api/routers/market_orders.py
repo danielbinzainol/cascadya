bca736ee422d06f5a3446a6223861cacd0b0c876
Loris Amabile <loris.amabile@cascadya.com>
2026-04-14 15:22:30 +0200

    remove useless test endpoints

--- src/ml_models/api/routers/market_orders.py
+++ src/ml_models/api/routers/market_orders.py
@@ -2,23 +2,11 @@ from fastapi import APIRouter, HTTPException
 from fastapi.responses import StreamingResponse
 
 from plots import plot_market_orders
-from src.ingest import data_workflow
 from src.market_orders import complex_market_orders_data_workflow
 from src.market_orders_paths import resolve_market_orders_csv_path
 
 
 router = APIRouter()
-
-
-@router.get("/plots/{project}/{data_type}/{filename}")
-def show_plots(project: str, data_type: str, filename: str):
-    df = data_workflow(project, data_type, filename)
-    return {"df to send to plot_workflow": df}
-
-
-@router.get("/message/{msg}")
-def change_msg(msg: str) -> dict:
-    return {"message": f"Hello World custom: {msg}"}
 
 
 @router.get("/market-orders/{project}/{data_type}/{filename}")
