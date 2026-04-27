d540f964fa64edb6cdb65c71bfcdf69cb2023f79
Loris Amabile <loris.amabile@cascadya.com>
2026-04-27 18:11:42 +0200

    move aeolus_publish_service out of bakcoffice/services

--- src/backoffice/services/__init__.py
+++ src/backoffice/services/__init__.py
@@ -1,28 +1,10 @@
 """Service layer for application business workflows."""
 
-from . import aeolus_publish_service
 from . import query_service
-from .aeolus_publish_service import (
-    DEFAULT_AEOLUS_BASE_URL,
-    DEFAULT_AEOLUS_TOKEN_URL,
-    DEFAULT_AEOLUS_WRITE_TRANSACTIONS_SCOPE,
-    PublishMarketOrdersResult,
-    publish_market_orders_workflow,
-    resolve_auth_config,
-    resolve_publish_csv_paths,
-)
 from .query_service import QueryValidationError, run_read_query
 
 __all__ = [
-    "DEFAULT_AEOLUS_BASE_URL",
-    "DEFAULT_AEOLUS_TOKEN_URL",
-    "DEFAULT_AEOLUS_WRITE_TRANSACTIONS_SCOPE",
-    "PublishMarketOrdersResult",
-    "aeolus_publish_service",
     "query_service",
-    "publish_market_orders_workflow",
     "QueryValidationError",
     "run_read_query",
-    "resolve_auth_config",
-    "resolve_publish_csv_paths",
 ]
